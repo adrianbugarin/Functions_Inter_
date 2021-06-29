7b0fe48153698f2e7313b1ec448733bb3574c3ab
Adrian_Bugarin <adrianbugarin3@gmail.com>
2021-06-29 17:32:40 -0600

    inital.commit

---
+++ source.py
@@ -0,0 +1,72 @@
+# 1.
+x = [ [5,2,3], [10,8,9] ] 
+students = [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'}
+]
+sports_directory = {
+    'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
+    'soccer' : ['Messi', 'Ronaldo', 'Rooney']
+}
+z = [ {'x': 10, 'y': 20} ]
+
+# # a
+x[1][0] = 15
+print(x)
+# # b
+sports_directory['basketball'][1] = 'Bryant'
+print(sports_directory)
+# c
+sports_directory['soccer'][0] = 'Andres'
+print(sports_directory)
+# d
+z[0]['y'] = 30
+print(z)
+
+# 2.
+students = [
+         {'first_name':  'Michael', 'last_name' : 'Jordan'},
+         {'first_name' : 'John', 'last_name' : 'Rosales'},
+         {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+         {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ] 
+for iterateDictionary in students:
+    print(iterateDictionary)
+
+# 3.
+
+def iterateDictionary2( first_name, last_name, students )
+    print first_name, students
+iterateDictionary2("Micheael","John","Mark","KB")
+
+
+dojo = {
+   'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+   'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+for printInfo 
+printInfo(dojo)
+# output:
+7 LOCATIONS
+San Jose
+Seattle
+Dallas
+Chicago
+Tulsa
+DC
+Burbank
+    
+8 INSTRUCTORS
+Michael
+Amy
+Eduardo
+Josh
+Graham
+Patrick
+Minh
+Devon
+
+
+
+
+
